f162b99fd750633a0e94bb9dbc6c8fc9d536cbaa
Kevin <kev@drule.org>
2013-12-26 02:22:56 -0700

    update per tutorial

--- src/drule.org/hello/hello.go
+++ src/drule.org/hello/hello.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+type Salutation struct {
+    name string
+    greeting string
+
+}
+
+func CreateMessage(name string, greeting ...string) (message string, alternate string) {
+    fmt.Println(len(greeting))
+    message = greeting[1] + " " + name
+    alternate =  "HEY! " + name
+    return
+}
+
+func Greet(sal Salutation) {
+    message, alternate := CreateMessage(sal.name, sal.greeting, "Yo!")
+    fmt.Println(message)
+    fmt.Println(alternate)
+
+}
+
 func main() {
-    fmt.Printf("hello, world\n")
+    var s = Salutation{"Bob", "hello"}
+    Greet(s)
 }
